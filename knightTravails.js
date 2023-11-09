bd1a17c3782b6cadc4609b831e70d66fe3c96c1c
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-11-09 15:21:25 +0700

    change some

--- knightTravails.js
+++ knightTravails.js
@@ -3,35 +3,37 @@ function knightMoves(from, to) {
 }
 
 function movesGraph() {
-  let obj = {};
-  for (let y = 0; y < 8; y++) {
-    for (let x = 0; x < 8; x++) {
-      obj[`${x}, ${y}`] = returnPossibleMoves(x, y);
+  let graph = {};
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      let vertex = returnPossibleMoves(x, y);
+      graph[`${x},${y}`] = vertex;
     }
   }
-  return obj;
+  return graph;
 }
 
 function returnPossibleMoves(x, y) {
   let arr = [];
-  arr.push([x + 2, y + 1]);
-  arr.push([x + 1, y + 2]);
-  arr.push([x + 2, y - 1]);
-  arr.push([x + 1, y - 2]);
-  arr.push([x - 2, y + 1]);
-  arr.push([x - 1, y + 2]);
-  arr.push([x - 2, y - 1]);
-  arr.push([x - 1, y - 2]);
+  arr.push(`${x + 2},${y + 1}`);
+  arr.push(`${x + 1},${y + 2}`);
+  arr.push(`${x + 2},${y - 1}`);
+  arr.push(`${x + 1},${y - 2}`);
+  arr.push(`${x - 2},${y + 1}`);
+  arr.push(`${x - 1},${y + 2}`);
+  arr.push(`${x - 2},${y - 1}`);
+  arr.push(`${x - 1},${y - 2}`);
+
   return arr.filter((n) => {
-    if (n[0] >= 0 && n[1] >= 0) {
-      if (n[0] < 8 && n[1] < 8) {
+    let coor = n.split(",");
+    if (coor[0] < 8 && coor[0] > 0) {
+      if (coor[1] < 8 && coor[1] > 0) {
         return true;
       }
     }
   });
 }
 
-console.log(movesGraph());
 function bfs(graph, start, end) {
   let queue = [];
   queue.push({

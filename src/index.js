6bae9b90a4d23880b77cd88a459f3cbea15a6345
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-11-13 14:25:52 +0700

    create func that animate the move

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import { createBoard } from "./display";
+import { knightMoves } from "./knightTravails";
+
 function clickHandler(e) {
   if (e.target.className.includes("square")) {
     start([e.target.dataset.row, e.target.dataset.col]);
+  }
+}
+function start(target) {
+  container.removeEventListener("click", clickHandler);
+  let moves = knightMoves(from, target);
+  let delay = 0;
+  for (let i = 0; i < moves[0].length; i++) {
+    if (i === moves[0].length - 1) {
+      setTimeout(() => {
+        createBoard(container, moves[0][i]);
+        container.addEventListener("click", clickHandler);
+      }, delay);
+      continue;
+    }
+    setTimeout(() => {
+      createBoard(container, moves[0][i]);
+    }, delay);
+    from = moves[0][i];
+    delay = delay + 500;
   }
 }
 const container = document.querySelector(".container");

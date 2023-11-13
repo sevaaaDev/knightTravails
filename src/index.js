fde5f8b309714e3ea53cf7c3137d1567e02549cf
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-11-13 14:25:10 +0700

    add listener to the container

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import { createBoard } from "./display";
+function clickHandler(e) {
+  if (e.target.className.includes("square")) {
+    start([e.target.dataset.row, e.target.dataset.col]);
+  }
+}
 const container = document.querySelector(".container");
 let from = [0, 0];
+container.addEventListener("click", clickHandler);
+
 createBoard(container, from);
